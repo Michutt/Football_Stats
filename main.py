5f3581220c0589d21c3ab7a393ddff11777bd97f
Michutt <niemc.michal@gmail.com>
2020-02-07 18:19:45 +0100

    main script

--- main.py
+++ main.py
@@ -1,9 +1,42 @@
 from PIL import Image, ImageDraw
+import math
+
+
+def PolygonCreate(listOfStats):
+    apexes = []
+    xMid = 400
+    yMid = 400
+    radius = 400
+    for i in range(len(listOfStats)):
+        pos = i * 360 / len(listOfStats)
+        x = int(math.sin(pos * math.pi / 180) * radius * listOfStats[i] + xMid)
+        y = int(math.cos(pos * math.pi / 180) * radius * listOfStats[i] + yMid)
+        apexes.append((x, y))
+
+    return apexes
+
 
 def main():
-    secondImg = Image.open("secondIMG.jpg")
-    
-    secondImg.save("final.jpg")
+    rando = []
+    rando.append(0.7)
+    rando.append(0.46)
+    rando.append(0.3)
+    rando.append(0.7)
+    rando.append(0.32)
+    rando.append(0.69)
+
+    firstImg = Image.open("firstIMG.png")
+    mask = Image.new("L", firstImg.size, 0)
+    draw = ImageDraw.Draw(mask)
+    apexes = PolygonCreate(rando)
+    draw.polygon(apexes, fill=255)
+    firstImg.putalpha(mask)
+    # firstImg.show()
+    finalImg = Image.open("secondIMG.png")
+    finalImg.paste(firstImg)
+    finalImg.show()
+    # firstImg.show()
+    finalImg.save("final.png")
 
 
 main()
